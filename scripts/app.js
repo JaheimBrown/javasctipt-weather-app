e0c7901b7d960c91fbd4b0e1e2620e5b103a142e
Kxngj <jaheimcbrown@gmail.com>
2021-11-12 00:25:13 -0500

    javascript weather app using local storage

---
+++ scripts/app.js
@@ -0,0 +1,64 @@
+const locationSearch = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('.time');
+const condition = document.querySelector('.condition')
+
+// Get City Details and Weather
+const getData = async (location) => {
+
+    const cityD = await getCity(location);
+    const weather = await getWeather(cityD.Key);
+
+    return { cityD, weather };
+}
+
+// Update UI
+const updateUI = (data) => {
+
+    const { cityD, weather } = data;
+    console.log(data);
+
+    // Setting Images & Icons
+    let timesrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timesrc);
+
+    condition.setAttribute('src', `img/icons/${weather.WeatherIcon}.svg`)
+
+    // Setting Details
+    details.innerHTML = `
+    <h5 class="my-3">${cityD.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    // Remove display none class upon getting data
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+}
+
+locationSearch.addEventListener('submit', e => {
+    // Prevent Default action
+    e.preventDefault();
+
+    // Get Form data
+    const location = locationSearch.city.value.trim();
+    locationSearch.reset();
+
+    // Getting Data
+    getData(location).then(data => updateUI(data))
+    .catch(err => console.log(err));
+
+    // local storage
+    localStorage.setItem('location', location);
+})
+
+if(localStorage.getItem('location')){
+    getData(localStorage.getItem('location'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+};
---
+++ scripts/forecast.js
@@ -0,0 +1,26 @@
+const key = 'I5NM45jAngHrpXPXEymuhbrjud1Y7IJR';
+
+// Get Weather Details
+const getWeather = async (id) => {
+    
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+// Get City Information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data =  await response.json();
+
+    return data[0];
+
+}
